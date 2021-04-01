95bb4fe3c31508bf48de2f6f5c0a4882c4dec35a
if317066 <rubenmanurung199@gmail.com>
2021-04-01 13:21:08 +0700

    add .env file

--- src/news/NewsContext.js
+++ src/news/NewsContext.js
@@ -5,10 +5,6 @@ export const NewsContext = createContext();
 
 export const NewsContextProvider = (props) => {
     const [data, setData] = useState();
-    // const apiKey = "32c9e60e573441b69a6c4da41ebd6fd0"; //1st account
-    const apiKey = "f10d00caae2a4260980971293dc09ef6"; //2nd account
-    // const apiKey = "2b476b42e5cd439caaf74722a69b993d"; //3nd account   
-    // const apiKey = "f4c8617ef03d45a49262e2f794a7a947"; //4nd account 
     const myNewsSession = sessionStorage.getItem('newsSession');
     let dateObj = new Date();
     let month = dateObj.getUTCMonth() + 1; //months from 1-12
@@ -20,7 +16,7 @@ export const NewsContextProvider = (props) => {
     
 
     useEffect(async () => {
-        await axios.get(`https://newsapi.org/v2/everything?q=${myNewsSession ? myNewsSession : "technology"}&from=${newdate}&to=${newdate}&sortBy=popularity&language=en&apiKey=${apiKey}`)
+        await axios.get(`https://newsapi.org/v2/everything?q=${myNewsSession ? myNewsSession : "technology"}&from=${newdate}&to=${newdate}&sortBy=popularity&language=en&apiKey=${process.env.REACT_APP_API_KEY}`)
         .then((response) => response.data ? setData(response.data) : document.write("Data is empty"))
         .catch((error) => document.write(`<center><h3>${error}</h3></center>`));
     }, []);
--- src/news/NewsSubTabsMenu.js
+++ src/news/NewsSubTabsMenu.js
@@ -5,21 +5,17 @@ export const NewsSubTabsMenu = createContext();
 
 export const NewsSubTabsMenuProvider = (props) => {
     const [data, setData] = useState();
-    // const apiKey = "32c9e60e573441b69a6c4da41ebd6fd0"; //1st account
-    const apiKey = "f10d00caae2a4260980971293dc09ef6"; //2nd account   
-    // const apiKey = "2b476b42e5cd439caaf74722a69b993d"; //3nd account   
-    // const apiKey = "f4c8617ef03d45a49262e2f794a7a947"; //4nd account   
     const mySubTab = sessionStorage.getItem('mySubTab');
     const myNewsSession = sessionStorage.getItem('newsSession');
 
     useEffect(async () => {
         myNewsSession === "business" 
         ?
-        await axios.get(`https://newsapi.org/v2/top-headlines?country=${mySubTab}&category=business&apiKey=${apiKey}`)
+        await axios.get(`https://newsapi.org/v2/top-headlines?country=${mySubTab}&category=business&apiKey=${process.env.REACT_APP_API_KEY}`)
         .then((response) => response.data ? setData(response.data) : console.log("Invalid") )
         .catch((error) => document.write(`<center><h3>${error}</h3></center>`))
         :   
-        await axios.get(`https://newsapi.org/v2/top-headlines?category=${myNewsSession ? myNewsSession : "technology"}&q=+${mySubTab ? mySubTab : "indonesia"}&apiKey=${apiKey}`)
+        await axios.get(`https://newsapi.org/v2/top-headlines?category=${myNewsSession ? myNewsSession : "technology"}&q=+${mySubTab ? mySubTab : "indonesia"}&apiKey=${process.env.REACT_APP_API_KEY}`)
         .then((response) => response.data ? setData(response.data) : console.log("Invalid") )
         .catch((error) => document.write(`<center><h3>${error}</h3></center>`));
     }, []);
